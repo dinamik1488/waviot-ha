b38c9451a6bcd9e4e5932f04911b8da2e82bd4e2
dinamik <76748623+dinamik1488@users.noreply.github.com>
2026-04-29 18:48:40 +0300

    Update const.py

--- custom_components/waviot/const.py
+++ custom_components/waviot/const.py
@@ -1,16 +1,1 @@
 DOMAIN = "waviot"
-
-SENSORS = {
-    "temperature": {"unit": "°C"},
-    "battery": {"unit": "V"},
-    "power_active": {"unit": "kW"},
-    "voltage_l1": {"unit": "V"},
-    "voltage_l2": {"unit": "V"},
-    "voltage_l3": {"unit": "V"},
-    "current_l1": {"unit": "A"},
-    "current_l2": {"unit": "A"},
-    "current_l3": {"unit": "A"},
-    "electricity_t1": {"unit": "kWh"},
-    "electricity_t2": {"unit": "kWh"},
-    "electricity_total": {"unit": "kWh"},
-}
