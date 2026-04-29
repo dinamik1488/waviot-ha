0dceb4cb092dfd552509143806bdfee51a1fcf2f
dinamik <76748623+dinamik1488@users.noreply.github.com>
2026-04-29 18:21:23 +0300

    Update sensor.py

--- custom_components/waviot/sensor.py
+++ custom_components/waviot/sensor.py
@@ -1,24 +1,14 @@
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-SENSORS = {
-    "temperature": "°C",
-    "battery": "V",
-    "power_active": "kW",
-    "voltage_l1": "V",
-    "voltage_l2": "V",
-    "voltage_l3": "V",
-    "current_l1": "A",
-    "current_l2": "A",
-    "current_l3": "A",
-    "electricity_total": "kWh",
-}
+SENSORS = ["temperature", "battery"]
 
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data["waviot"][entry.entry_id]
 
-    async_add_entities(
-        [WaviotSensor(coordinator, k) for k in SENSORS]
-    )
+    async_add_entities([
+        WaviotSensor(coordinator, key) for key in SENSORS
+    ])
+
 
 class WaviotSensor(CoordinatorEntity):
 
@@ -27,23 +17,15 @@ class WaviotSensor(CoordinatorEntity):
         self.key = key
 
     @property
-    def unique_id(self):
-        return f"waviot_{self.key}"
-
-    @property
     def name(self):
         return f"WAVIoT {self.key}"
 
     @property
-    def state(self):
-        try:
-            return (
-                self.coordinator.data["modem"].get(self.key)
-                or self.coordinator.data["values"].get(self.key, {}).get("last_value")
-            )
-        except:
-            return None
+    def unique_id(self):
+        return f"waviot_{self.key}"
 
     @property
-    def unit_of_measurement(self):
-        return SENSORS[self.key]
+    def state(self):
+        return (
+            self.coordinator.data.get("modem", {}).get(self.key)
+        )
