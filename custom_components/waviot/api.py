3c9547ef71066b4891cfd1a617c50fb5a6a883bc
dinamik <76748623+dinamik1488@users.noreply.github.com>
2026-04-29 18:49:10 +0300

    Update api.py

--- custom_components/waviot/api.py
+++ custom_components/waviot/api.py
@@ -1,18 +1,19 @@
 import aiohttp
 
 class WaviotAPI:
+
     def __init__(self, modem_id, api_key):
         self.modem_id = modem_id
         self.api_key = api_key
 
     async def get_modem(self):
         url = f"https://lk.waviot.ru/api.modem/info/?id={self.modem_id}&key={self.api_key}"
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as r:
+        async with aiohttp.ClientSession() as s:
+            async with s.get(url) as r:
                 return await r.json()
 
     async def get_values(self):
         url = f"https://lk.waviot.ru/api.data/get_values/?modem_id={self.modem_id}&key={self.api_key}&limit=1"
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as r:
+        async with aiohttp.ClientSession() as s:
+            async with s.get(url) as r:
                 return await r.json()
