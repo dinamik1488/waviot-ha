fe3e55758f9de8d54c547be9dba6a1132b76ba4f
dinamik <76748623+dinamik1488@users.noreply.github.com>
2026-04-29 18:50:01 +0300

    Update __init__.py

--- custom_components/waviot/__init__.py
+++ custom_components/waviot/__init__.py
@@ -1,7 +1,7 @@
 from .coordinator import WaviotCoordinator
 
-
 async def async_setup_entry(hass, entry):
+
     coordinator = WaviotCoordinator(
         hass,
         entry.data["modem_id"],
@@ -11,7 +11,11 @@ async def async_setup_entry(hass, entry):
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault("waviot", {})
-    hass.data["waviot"][entry.entry_id] = coordinator
+    hass.data["waviot"][entry.entry_id] = {
+        "coordinator": coordinator,
+        "tariff_day": entry.data["tariff_day"],
+        "tariff_night": entry.data["tariff_night"],
+    }
 
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
@@ -19,7 +23,10 @@ async def async_setup_entry(hass, entry):
 
 
 async def async_unload_entry(hass, entry):
+
     unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+
     if unload_ok:
         hass.data["waviot"].pop(entry.entry_id)
+
     return unload_ok
